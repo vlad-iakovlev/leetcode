980e0068a4dffdc4522b8191c3f19ae95000f6f9
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-25 23:33:54 +0400

    Create 3-longest-substring-without-repeating-characters.ts

---
+++ solutions/3-longest-substring-without-repeating-characters.ts
@@ -0,0 +1,22 @@
+function lengthOfLongestSubstring(s: string): number {
+    let maxLen = 0;
+
+    let i = 0;
+    let j = 0;
+
+    while (i < s.length, j < s.length) {
+        const substr = s.slice(i, j)
+        
+        if (substr.includes(s[j])) {
+            i++
+        } else {
+            if (maxLen < substr.length + 1) {
+                maxLen = substr.length + 1
+            }
+
+            j++
+        }
+    }
+
+    return maxLen
+}
