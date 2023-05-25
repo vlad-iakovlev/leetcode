5d57f9ba8e54d44652b3ee734d11653bfe3545d4
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-26 03:54:31 +0400

    Create 11-container-with-most-water.ts

---
+++ solutions/11-container-with-most-water.ts
@@ -0,0 +1,16 @@
+function maxArea(height: number[]): number {
+    let result = 0
+
+    let i = 0
+    let j = height.length - 1
+
+    while (i < j) {
+        const amount = (j - i) * Math.min(height[i], height[j])
+        if (amount > result) result = amount
+
+        if (height[i] < height[j]) i++
+        else j--
+    }
+
+    return result
+};
