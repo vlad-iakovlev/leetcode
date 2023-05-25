b725474b51a39e410e80a2b8e767610e7025bb6a
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-26 01:36:57 +0400

    Update 7-reverse-integer.ts

--- solutions/7-reverse-integer.ts
+++ solutions/7-reverse-integer.ts
@@ -1,32 +1,15 @@
-function reverse(x: number): number {
-    if (x > 0) return reversePositive(x)
-    if (x < 0) return reverseNegative(x)
-    return x
-}
+const max = Math.floor(Math.pow(2, 31) / 10)
 
-function reversePositive(x: number): number {
+function reverse(x: number): number {
     let result = 0
 
     while (x) {
         const digit = x % 10
-        x = Math.floor(x / 10)
+        x = x > 0 ? Math.floor(x / 10) : Math.ceil(x / 10)
         if (result > max || result === max && digit > 7) return 0
+        if (result < -max || result === -max && digit < -8) return 0
         result = result * 10 + digit
     }
 
     return result
-}
-
-function reverseNegative(x: number): number {
-    x = -x
-    let result = 0
-
-    while (x) {
-        const digit = x % 10
-        x = Math.floor(x / 10)
-        if (result > max || result === max && digit > 8) return 0
-        result = result * 10 + digit
-    }
-
-    return -result
 }
