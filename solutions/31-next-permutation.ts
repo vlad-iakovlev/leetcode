a7f8f710b8453d94be8464212957d42de52e0b0e
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-27 00:56:26 +0400

    Create 31-next-permutation.ts

---
+++ solutions/31-next-permutation.ts
@@ -0,0 +1,22 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function nextPermutation(nums: number[]): void {
+    let i = nums.length - 2
+    for (; i >= 0 && nums[i] >= nums[i + 1]; i--) {}
+
+    if (i >= 0) {
+        let j = i;
+        for (; j < nums.length - 1 && nums[j + 1] > nums[i]; j++) {}
+        const val = nums[i]
+        nums[i] = nums[j]
+        nums[j] = val
+    }
+
+    i++
+    for (let j = nums.length - 1; i < j; i++, j--) {
+        const val = nums[i]
+        nums[i] = nums[j]
+        nums[j] = val
+    }
+}
