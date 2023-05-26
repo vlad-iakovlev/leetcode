29e8d95bfa102a91d137887108f154ba9d880383
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-26 15:43:47 +0400

    Create 19-remove-nth-node-from-end-of-list.ts

---
+++ solutions/19-remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(
+    head: ListNode | null,
+    n: number
+): ListNode | null {
+    return removeNthFronEndInternal(head, n).head
+};
+
+function removeNthFronEndInternal(
+    head: ListNode | null,
+    n: number
+): { nthFronEnd: number, head: ListNode | null} {
+    if (!head) return { nthFronEnd: 0, head }
+
+    const next = removeNthFronEndInternal(head.next, n)
+    if (next.nthFronEnd + 1 === n) head = next.head
+    else head.next = next.head
+
+    return { nthFronEnd: next.nthFronEnd + 1, head }
+}
