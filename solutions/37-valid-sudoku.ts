da967f2e0c560b738ddd1755cf7d36d887e2435e
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-27 05:00:24 +0400

    Create 37-valid-sudoku.ts

---
+++ solutions/37-valid-sudoku.ts
@@ -0,0 +1,25 @@
+function isValidSudoku(board: string[][]): boolean {
+    let rows: number[] = []
+    let cols: number[] = []
+    let boxes: number[] = []
+    
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (board[row][col] === '.') continue
+
+            const value = 1 << Number(board[row][col])
+
+            if (rows[row] & value) return false
+            rows[row] |= value
+
+            if (cols[col] & value) return false
+            cols[col] |= value
+
+            const box = Math.floor(row / 3) * 3 + Math.floor(col / 3)
+            if (boxes[box] & value) return false
+            boxes[box] |= value
+        }
+    }
+
+    return true
+};
