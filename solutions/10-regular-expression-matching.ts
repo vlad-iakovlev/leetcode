de0cf06e2dad336eb2e377b27802dfb559168d9c
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-28 02:17:24 +0400

    Update 10-regular-expression-matching.ts

--- solutions/10-regular-expression-matching.ts
+++ solutions/10-regular-expression-matching.ts
@@ -1,28 +1,27 @@
 function isMatch(s: string, p: string): boolean {
-    const n = s.length
-    const m = p.length
+    const n = s.length + 1
+    const m = p.length + 1
 
-    const dp = Array<boolean>((n + 1) * (m + 1))
-    const k = (i: number, j: number) => i * (m + 1) + j
-    dp[k(0, 0)] = true
+    const dp = Array<boolean>(n * m)
+    dp[0] = true
 
-    for (let i = 0; i <= n; i++) {
-        for (let j = 1; j <= m; j++) {
-            dp[k(i, j)] =
+    for (let i = 0; i < n; i++) {
+        for (let j = 1; j < m; j++) {
+            dp[i * m + j] =
                 p[j - 1] === '*' ? (
-                    dp[k(i, j - 2)] ||
+                    dp[i * m + j - 2] ||
                     (
                         i > 0 &&
                         (p[j - 2] === '.' || s[i - 1] === p[j - 2]) &&
-                        dp[k(i - 1, j)]
+                        dp[(i - 1) * m + j]
                     )
                 ) : (
                     i > 0 &&
-                    dp[k(i - 1, j - 1)] &&
+                    dp[(i - 1) * m + j - 1] &&
                     (p[j - 1] === '.' || s[i - 1] === p[j - 1])
                 )
         }
     }
 
-    return dp[k(n, m)] ?? false
+    return dp[n * m - 1] ?? false
 }
