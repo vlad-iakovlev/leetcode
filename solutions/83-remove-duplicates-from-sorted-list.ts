53b4575c3ac2ff5862187e3b246396368ac65e9b
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-30 01:47:24 +0400

    Create 83-remove-duplicates-from-sorted-list.ts

---
+++ solutions/83-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    if (!head) return null
+    while (head.val === head.next?.val) head = head.next
+    head.next = deleteDuplicates(head.next)
+    return head
+}
