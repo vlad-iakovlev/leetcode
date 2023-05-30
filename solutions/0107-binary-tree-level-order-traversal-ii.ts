c65a42c890eb5181896079d223aa99c908b77fc6
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-31 03:18:39 +0400

    Create 0107-binary-tree-level-order-traversal-ii.ts

---
+++ solutions/0107-binary-tree-level-order-traversal-ii.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+    const result: number[][] = []
+    const deq = Array<[number, TreeNode | null]>([0, root])
+
+    while (deq.length) {
+        const [level, node] = deq.shift()
+
+        if (node) {
+            if (result[level]) result[level].push(node.val)
+            else result[level] = [node.val]
+            deq.push([level + 1, node.left], [level + 1, node.right])
+        }
+    }
+
+    result.reverse()
+
+    return result
+}
