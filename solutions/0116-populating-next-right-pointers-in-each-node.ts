9ec2a4f77d390f0e5ece4b52a56fd81162dc1f7d
Vladislav Yakovlev <vlad309523@gmail.com>
2023-06-01 11:24:52 +0400

    Create 0116-populating-next-right-pointers-in-each-node.ts

---
+++ solutions/0116-populating-next-right-pointers-in-each-node.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     left: Node | null
+ *     right: Node | null
+ *     next: Node | null
+ *     constructor(val?: number, left?: Node, right?: Node, next?: Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function connect(root: Node | null): Node | null {
+    function traverse(nodes: Array<Node | null>) {
+        const nextNodes: Array<Node | null> = []
+
+        for (let i = 0; i < nodes.length; i++) {
+            if (!nodes[i]) return
+            nodes[i].next = nodes[i + 1] ?? null
+            nextNodes.push(nodes[i].left, nodes[i].right)
+        }
+
+        traverse(nextNodes)
+    }
+
+    traverse([root])
+
+    return root
+}
