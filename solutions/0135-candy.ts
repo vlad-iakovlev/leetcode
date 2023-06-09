b6c9135458c7bb2acbc8742d3bea34e7c4d6dd76
Vladislav Yakovlev <vlad309523@gmail.com>
2023-06-10 02:48:23 +0400

    Update 0135-candy.ts

--- solutions/0135-candy.ts
+++ solutions/0135-candy.ts
@@ -2,17 +2,16 @@ function candy(ratings: number[]): number {
     const n = ratings.length
     const candies = Array<number>(n)
 
-    candies[0] = 1
-    for (let i = 1; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         if (ratings[i] > ratings[i - 1]) candies[i] = candies[i - 1] + 1
         else candies[i] = 1
     }
 
-    for (let i = n - 2; i >= 0; i--) {
-        if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) candies[i] = candies[i + 1] + 1
+    let result = 0
+    for (let i = n - 1; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) candies[i] = Math.max(candies[i], candies[i + 1] + 1)
+        result += candies[i]
     }
 
-    console.log(candies)
-
-    return candies.reduce((acc, count) => acc + count, 0)
+    return result
 }
