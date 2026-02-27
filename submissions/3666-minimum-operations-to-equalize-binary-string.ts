9e01e453a27774e6f8d54641a7b23fabeb10580d
Vladislav Yakovlev <vlad309523@gmail.com>
2026-02-27 21:57:39 +0100

    feat: 3666 solution

---
+++ submissions/3666-minimum-operations-to-equalize-binary-string.ts
@@ -0,0 +1,34 @@
+function minOperations(s: string, k: number): number {
+  let zero = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "0") zero++;
+  }
+
+  if (!zero) return 0;
+
+  if (s.length === k) return zero === s.length ? 1 : -1;
+
+  let res = Infinity;
+
+  if (isOdd(k) === isOdd(zero)) {
+    const odd = Math.max(
+      Math.ceil(zero / k),
+      Math.ceil((s.length - zero) / (s.length - k)),
+    );
+    res = Math.min(res, odd + isEven(odd));
+  }
+
+  if (isEven(zero)) {
+    const even = Math.max(
+      Math.ceil(zero / k),
+      Math.ceil(zero / (s.length - k)),
+    );
+    res = Math.min(res, even + isOdd(even));
+  }
+
+  if (Number.isFinite(res)) return res;
+  return -1;
+}
+
+const isOdd = (num: number) => num & 1;
+const isEven = (num: number) => (num & 1) ^ 1;
