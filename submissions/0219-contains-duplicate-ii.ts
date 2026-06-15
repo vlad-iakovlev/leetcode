0eb06cada51c10dae30ff70ac8e891fe71201700
Vladislav Iakovlev <vlad309523@gmail.com>
2026-06-15 13:44:43 +0200

    feat: 0219 solution

---
+++ submissions/0219-contains-duplicate-ii.ts
@@ -0,0 +1,11 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  const set = new Set<number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+    if (i >= k) set.delete(nums[i - k]);
+  }
+
+  return false;
+}
